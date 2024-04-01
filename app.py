9c8a519d859bf4e222256cc25d47b4f7dc33b44e
LilMac1999 <165190563+LilMac1999@users.noreply.github.com>
2024-04-01 15:01:58 -0300

    Add files via upload
    App as it stands now

---
+++ app.py
@@ -0,0 +1,27 @@
+import os
+
+from flask import Flask, redirect, render_template, request, url_for
+from openai import OpenAI
+client = OpenAI(
+    api_key=os.environ.get(".env"),
+)
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=("GET", "POST"))
+def index():
+    if request.method == "POST":
+        reflection = request.form["reflection"]
+        response = client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            temperature=0.6,
+            messages=[
+                {"role": "system","content": "Users will be able to ask for ideas or concepts be explained to them in simple terms as if explaing them to someone who is 12 years of age. For example explaing the plot of famous litary works or what and an idea like what a geopolitical economy is"},
+                {"role": "user", "content": question and answer}
+            ]
+        )
+        return redirect(url_for("index", result=response.choices[0].message.content))
+
+    result = request.args.get("result")
+    return render_template("index.html", result=result)
